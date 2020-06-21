92dcc7927af53ad0caa631fb97c5955d68917b0b
Crow77 <djcrow77@gmail.com>
2020-06-21 16:39:02 +0100

    Update pipeline_script.py

--- pipeline_script.py
+++ pipeline_script.py
@@ -7,59 +7,82 @@ import argparse
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.abspath(os.path.join(CURRENT_DIR, 'frames'))
 
-def exec_PWC_Net(metodo, img1, img2):
-	
+def video_denoising():
+	pass
 
-	with open('{}/img1_PWC_Net.txt'.format(CURRENT_DIR), 'w') as f:
-		f.write('{}{} \n'.format('./frames/', img1))
-		f.write('{}{} '.format('./frames/', img2))
+def exec_PWC_Net(metodo, images):
 
-	with open('{}/img2_PWC_Net.txt'.format(CURRENT_DIR), 'w') as f:
-		f.write('{}{} \n'.format('./frames/', img2))
-		f.write('{}{} '.format('./frames/', img1))
+	flag = True
+
+	with open('{}/10_PWC_Net.txt'.format(CURRENT_DIR), 'w') as f:   #open() works as try-Except
+		for img in images:
+			f.write('{}{} \n'.format('./frames/', img))
+
+	with open('{}/11_PWC_Net.txt'.format(CURRENT_DIR), 'w') as f:
+		for img in reversed(images):
+			f.write('{}{} \n'.format('./frames/', img))
 
 	with open('{}/out.txt'.format(CURRENT_DIR), 'w') as f:
-		f.write('{}{}{}\n'.format('./tmp/',img1[:-4],'_forward.flo'))
-		f.write('{}{}{} '.format('./tmp/',img2[:-4],'_backward.flo'))
+		for img in images:
+			if flag:
+				f.write('{}{}{}\n'.format('./tmp/',img[:-4],'_forward.flo'))
+				flag = False
+			else:
+				f.write('{}{}{}\n '.format('./tmp/',img[:-4],'_backward.flo'))
+				flag = True
 
-	os.system('./proc_images img1_PWC_Net.txt img2_PWC_Net.txt out.txt') #Ejecuta PWC_Net y genera flujo óptico
+	os.system('./proc_images 10_PWC_Net.txt 11_PWC_Net.txt out.txt') #run PWC_Net & generate flow. proc_images is the generated binary
 
 
 
-def exec_LiteFlowNet(metodo, img1, img2):
+def exec_LiteFlowNet(metodo, images):
 
+	with open('{}/10_LiteFlowNet.txt'.format(CURRENT_DIR), 'w') as f:
+		for img in images:
+			if '10_' == img[0:3]:
+				f.write('{}{} \n'.format('./frames/', img))
 
-	with open('{}/img1_LiteFlowNet.txt'.format(CURRENT_DIR), 'w') as f:
-		f.write('{}{} \n'.format('./frames/', img1))
-
-	with open('{}/img2_LiteFlowNet.txt'.format(CURRENT_DIR), 'w') as f:
-		f.write('{}{} \n'.format('./frames/', img2))
+	with open('{}/11_LiteFlowNet.txt'.format(CURRENT_DIR), 'w') as f:
+		for img in images:
+			if '11_' == img[0:3]:
+				f.write('{}{} \n'.format('./frames/', img))
 	
-	os.system('./test_iter img1_LiteFlowNet.txt img2_LiteFlowNet.txt results') #Ejecuta LiteFlowNet y genera flujo óptico	
+	os.system('./test_iter 10_LiteFlowNet.txt 11_LiteFlowNet.txt results') #run LiteFlowNet & generate flow. test_iter is the generated binary	
 	
 
 
 
 if __name__ == '__main__':
-	#Inicializar el parser
+	#Initialize the parser
 	parser = argparse.ArgumentParser(
 		description ="Demo en linea para video denoising usando flujo óptico"
 	)
 
-	#Parámetros posicional/opcional
-	parser.add_argument('metodo', help="Mátodo a ejecutar: PWC_Net/LiteFlowNet")  
-	parser.add_argument('img1', help="Proporcione imagen1")
-	parser.add_argument('img2', help="Proporcione imagen2")
+	#positional/optional parameter
+	parser.add_argument('method', help="Mátodo a ejecutar: PWC_Net/LiteFlowNet")  
+	parser.add_argument('total_img', help="Proporcione imagen1", type = int)
 
-
-	#Parsear los argumentos
+	#Parse the arguments
 	args = parser.parse_args()
+
+	#Get images clasified by tag
+	images10_ = [image for image in sorted(os.listdir(DATA_DIR)) if '10_' in image]
+	images11_ = [image for image in sorted(os.listdir(DATA_DIR)) if '11_' in image]
 	
-	
-	if args.metodo == 'PWC_Net':
-		exec_PWC_Net(args.metodo, args.img1, args.img2)
-	elif args.metodo == 'LiteFlowNet':
-		exec_LiteFlowNet(args.metodo, args.img1, args.img2)
+
+	list_img = []
+	count = 1
+	#Creating a list with the amount o images passed by parameter 
+	for x,y in zip(images10_, images11_):
+		if count <= int(args.total_img):
+			list_img.append(x)
+			list_img.append(y)
+			count += 1
+		
+	if args.method == 'PWC_Net':
+		exec_PWC_Net(args.method, list(list_img))
+	elif args.method == 'LiteFlowNet':
+		exec_LiteFlowNet(args.method, list(list_img))
 	else:
-		print("Proporcione un método válido")
+		print("Provide a valid method")
 	
